6f17eb7e794af207b22f1dca21dc4532cb261b02
leonardo chaves <leonardoaschaves@gmail.com>
2025-05-27 23:33:10 -0300

    fix: correct session retrieval for 'senha' in HandleInertiaRequests middleware

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -42,7 +42,7 @@ class HandleInertiaRequests extends Middleware
             'flash' => [
                 'success' => fn () => $request->session()->get('success'),
                 'error' => fn () => $request->session()->get('error'),
-                'senha' => fn () => session('senha'),
+                'senha' => fn () => $request->session()->get('senha'),
             ],
         ]);
     }
